acc2387070343e48a9abb7d8113b9564fe76ffa8
Kristina <77446626+Nettinka@users.noreply.github.com>
2021-06-03 21:41:01 +0300

    Update main.js
    Исправила по последнему таску. Плюс поместила игроков в див аренас. Не могу понять, почему этот див уползает вниз после создания игроков.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log('Hi');
 const box = document.getElementById('box');
-const $arenas = document.createElement('div');
-$arenas.classList.add('arenas');
+//const $arenas = document.createElement('div');
+const $arenas1 = document.querySelector('.arenas');
+//$arenas.classList.add('arenas');
 const $root = document.querySelector('.root');
 
 const player1 = {
@@ -23,39 +24,38 @@ const player2 = {
     }
 }
 
-function createPlayer(player1){
+function createPlayer(playerClass, player){
     const $player1 = document.createElement('div');
-    $player1.classList.add('player1');
+    $player1.classList.add(playerClass);
 
     const $progressbar = document.createElement('div');
     $progressbar.classList.add('progressbar');
 
     const $life = document.createElement('div');
     $life.classList.add('life');
-    $life.innerText = player1.hp;
+    $life.innerText = player.hp;
     $life.style.width = '100%';
     $progressbar.appendChild($life);
     
     $player1.appendChild($progressbar);
     const $name = document.createElement('div');
-    $name.innerText = player1.name;
+    $name.innerText = player.name;
     $progressbar.appendChild($name);
 
     const $character = document.createElement('div');
     $character.classList.add('character');
     const $img = document.createElement('img');
-    $img.src = player1.img;
+    $img.src = player.img;
     $character.appendChild($img);
     
     $player1.appendChild($progressbar);
     $player1.appendChild($character);
 
     
-    $arenas.appendChild($player1);
+    $arenas1.appendChild($player1);
 
-    
-    $root.appendChild($arenas);
+    $root.appendChild($arenas1);
 }
-createPlayer(player1);
-createPlayer(player2);
+createPlayer('player1', player1);
+createPlayer('player2', player2);
 console.log(player1.attack());
